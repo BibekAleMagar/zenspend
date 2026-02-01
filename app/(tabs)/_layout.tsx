d5ab85917747267723c2e0afd2510ed7174d2cc8
Bibek Ale Magar <magarbibek71@gmail.com>
2026-02-01 21:54:21 +0545

    adds native wind in project

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,34 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+import { HapticTab } from "@/components/haptic-tab";
+import { IconSymbol } from "@/components/ui/icon-symbol";
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <SafeAreaView className="flex-1" edges={["top"]}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+          headerShown: false,
+          tabBarButton: HapticTab,
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => (
+              <IconSymbol size={28} name="house.fill" color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </SafeAreaView>
+  );
+}
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { Image } from "expo-image";
+import { Platform, StyleSheet } from "react-native";
+
+import { HelloWave } from "@/components/hello-wave";
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { Link } from "expo-router";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+export default function Index() {
+  return (
+    <>
+      <Text className="text-2xl text-red-600">Hi Hello</Text>
+    </>
+  );
+}
